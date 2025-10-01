9b80349fd1c8c8304535b01d9f6cfe69a7cb787f
Kabir Mohammed <kabirwale@yahoo.com>
2025-10-01 11:59:43 +0200

    project file

---
+++ monitor.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PIPELINE_DIR=~/data_pipeline
+LOG_DIR=$PIPELINE_DIR/logs
+LOG_FILE=$LOG_DIR/preprocess.log
+SUMMARY_LOG=$LOG_DIR/monitor_summary.log
+
+mkdir -p "$LOG_DIR"
+
+
+ERRORS=$(grep -iE "ERROR|failed" $LOG_FILE)
+
+if [ -n "$ERRORS" ]; then
+    echo "Errors found in logs:"
+    echo "$ERRORS"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - ERRORS DETECTED" >> "$SUMMARY_LOG"
+    echo "$ERRORS" >> "$SUMMARY_LOG"
+else
+    echo "No errors detected in logs."
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - No errors found" >> "$SUMMARY_LOG"
+fi
---
+++ preprocess.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+PIPELINE_DIR=~/data_pipeline
+INPUT_DIR=$PIPELINE_DIR/input
+OUTPUT_DIR=$PIPELINE_DIR/output
+API_URL="https://raw.githubusercontent.com/dataengineering-community/launchpad/refs/heads/main/Linux/sales_data.csv"
+CSV_FILE="$INPUT_DIR/sales_data.csv"
+INPUT_FILE=$INPUT_DIR/sales_data.csv
+LOG_DIR=$PIPELINE_DIR/logs
+OUTPUT_FILE=$OUTPUT_DIR/cleaned_sale_data.csv
+LOG_FILE=$LOG_DIR/preprocess.log
+
+# Create directories
+mkdir -p "$PIPELINE_DIR"
+mkdir -p "$INPUT_DIR" "$LOG_DIR" "$OUTPUT_DIR"
+
+echo "Downloading sales data from API..."
+if wget "$API_URL" -q -O "$CSV_FILE"; then
+    echo "Download successful. File saved to $CSV_FILE"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - SUCCESS: CSV Data Downloaded file from $API_URL" >> "$LOG_FILE"
+
+    # Preprocess CSV: Remove rows with failed in the last column and drop the last column
+    echo "Preprocessing data..."
+    awk 'BEGIN{FS=OFS=","} {
+    NF--;
+    if (NR==1 || tolower($NF) != "failed")
+        print
+    }' "$INPUT_FILE" > "$OUTPUT_FILE"
+    echo "Preprocessing complete. Cleaned file saved at $OUTPUT_FILE"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - SUCCESS: Data Preprocessed $INPUT_FILE -> $OUTPUT_FILE" >> "$LOG_FILE"
+else
+    echo "Download failed from $API_URL"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR: CSV Data Download failed from $API_URL" >> "$LOG_FILE"
+    exit 1
+fi
